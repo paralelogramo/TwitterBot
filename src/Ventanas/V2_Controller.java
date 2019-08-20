be2ec316ceffe4192f10276adf355dab180932b8
Vicente <vrojasaranda@gmail.com>
2019-08-20 03:18:35 -0400

    Cambios en V2
    ahora el programa envía tweets en formato de texto y con imagenes, ademas es capaz de borrar tweets pero solo por consola

--- src/Ventanas/V2_Controller.java
+++ src/Ventanas/V2_Controller.java
@@ -18,17 +18,30 @@ package Ventanas;
 
 import Clases.ControlVentana;
 import Clases.Mensaje;
+import java.io.File;
 import javafx.scene.control.TextArea;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
@@ -42,7 +55,13 @@ import twitter4j.conf.ConfigurationBuilder;
 public class V2_Controller extends ControlVentana implements Initializable {
     
     @FXML TextArea msj;
+    @FXML Text user;
+    @FXML Text email;
+    @FXML Text notificationImage;
     @FXML ProgressIndicator pgA;
+    @FXML ImageView profilePhoto;
+    private File imgFile;
+    private Stage stage = this.stage;
     private Twitter twitter;
     
     public void progresoTexto(KeyEvent event){      
@@ -51,14 +70,63 @@ public class V2_Controller extends ControlVentana implements Initializable {
         pgA.setProgress(msj.getText().length()/280.0);
     }
     
-    public void enviarMensaje(MouseEvent event) throws TwitterException{       
+    public int sendTwitter(MouseEvent event) throws TwitterException{       
         Mensaje mensaje = new Mensaje();
         mensaje.setMensaje(msj.getText());              
-        if(mensaje.verificar()){
-            twitter.updateStatus(mensaje.getMensaje());
-            msj.clear();
+        StatusUpdate status = new StatusUpdate(mensaje.getMensaje());
+        if (imgFile != null) {
+            status.setMedia(imgFile);
+            twitter.updateStatus(status);
+            notificationImage.setVisible(false);
+            return 0;
         }
-    }         
+        if(mensaje.verificar() && imgFile == null){
+            twitter.updateStatus(status);
+            msj.clear();
+            notificationImage.setVisible(false);
+        }
+        return 0;
+    }
+    
+    public void upImage_Twitter(MouseEvent event) throws TwitterException{
+        FileChooser filech = new FileChooser();
+        filech.setTitle("Buscar Imagen");
+        filech.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+        imgFile = filech.showOpenDialog(stage);
+        if (imgFile != null) {
+            notificationImage.setVisible(true);
+        }
+    }
+    
+    public void deleteTwitter(MouseEvent event) throws TwitterException{
+        System.out.println("Entro");
+        ResponseList<Status> Line = twitter.getHomeTimeline();
+        for (Status status : Line) {
+            System.out.println("ID: " + status.getId() + " - " + status.getText());
+        }
+        System.out.print("Id del status a eliminar: ");
+        Scanner sc = new Scanner(System.in);
+        
+        long id = sc.nextLong();
+        
+        for (Status status : Line) {
+            if (id==status.getId()) {
+                twitter.destroyStatus(id);
+                System.out.println("status eliminado");
+            }
+        }
+    }
+    
+    public void followUser(MouseEvent event) throws TwitterException{
+        
+    }
+    
+    public void unfollowUser(MouseEvent event) throws TwitterException{
+        
+    }
     
 
     /**
@@ -73,8 +141,24 @@ public class V2_Controller extends ControlVentana implements Initializable {
         .setOAuthConsumerKey("FI7zwXVxcqKBAozpTrqxJNfIA")
         .setOAuthConsumerSecret("bNjRO185DPOXnFqMOXJ2arJOQF1EG3TLFKXQPGCoCrM7649Lpe")
         .setOAuthAccessToken("1160640022971867138-WMcI6McTZAjpQ81949F6pr8NhfyO5a")
-        .setOAuthAccessTokenSecret("2YtfMfSlpMUO3QlS3adHSw9AJmTqJMXiZC9D4BEcyhP7Q");
+        .setOAuthAccessTokenSecret("2YtfMfSlpMUO3QlS3adHSw9AJmTqJMXiZC9D4BEcyhP7Q")
+        .setIncludeEmailEnabled(true);
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter = tf.getInstance();
+        
+        try {
+            User newUser = twitter.showUser(twitter.getScreenName());            
+            user.setText("@"+newUser.getName());
+            /*
+            email.setText(newUser.getEmail());
+            String urlIm = newUser.getProfileImageURL();
+            Image img = new Image(urlIm);
+            profilePhoto.setImage(img);
+
+            PENDIENTES POR PROBLEMAS CON PRIVACY POLICY
+            */
+        } catch (TwitterException | IllegalStateException ex) {
+            Logger.getLogger(V2_Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }        
 }
